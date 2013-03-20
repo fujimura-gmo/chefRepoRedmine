acbedc5af5c5b2305648610501242367b41e417a
Arata Fujimura <aratafuji@gmail.com>
2013-03-20 17:50:01 +0900

    change

--- site-cookbooks/redmine/recipes/default.rb
+++ site-cookbooks/redmine/recipes/default.rb
@@ -14,8 +14,24 @@ git "/var/lib/redmine" do
   group "root"
 end
 
+bash "mysql-setting" do
+  code <<-EOC
+    echo "CREATE DATABASE redmine DEFAULT CHARACTER SET utf8;" | mysql -uroot -piloverandompasswordsbutthiswilldo;
+    echo "GRANT ALL ON redmine.* TO redmine@'localhost' IDENTIFIED BY 'redmine';" | mysql -uroot -piloverandompasswordsbutthiswilldo;
+    echo "FLUSH PRIVILEGES;" | mysql -uroot -piloverandompasswordsbutthiswilldo;
+  EOC
+end
+
 bash "install-bundle" do
   code <<-EOC
     gem install bundler
   EOC
 end
+
+bash "exec-bundle" do
+  code <<-EOC
+    cd /var/lib/redmine;
+    bundle exec install --path vendor/bundle --without development test postgresql sqlite;
+  EOC
+end
+
